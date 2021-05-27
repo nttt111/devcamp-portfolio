4a93d081033535387ff84b5806c1796280434717
Nan Tint Tint Tun <nantinttinttun.gic@gmail.com>
2021-05-27 22:46:07 +0630

    Implemeted destory action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,4 +43,16 @@ class PortfoliosController < ApplicationController
     @portfolio_item =Portfolio.find(params[:id])
   end
 
+def destroy
+  #Perform the lookup
+  @portfolio_item =Portfolio.find(params[:id])
+  #Destroy/Delete the Recort
+    @portfolio_item.destroy
+
+  #Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Record was Remove" }
+ end
+end
+
 end
