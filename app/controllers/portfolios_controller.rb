9134fac647d936e939a0f9754c18cf6280b91856
Nan Tint Tint Tun <nantinttinttun.gic@gmail.com>
2021-06-01 11:51:41 +0630

    Implemented partials with local dataf for portfikui form and v

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,64 +1,73 @@
 class PortfoliosController < ApplicationController
-layout 'portfolio'
+  layout 'portfolio'
+  
+  def index
+    @portfolio_items = Portfolio.all
+  end
 
-	def index
-		@portfolio_items = Portfolio.all
-	end
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
 
-	def new
-		@portfolio_item = Portfolio.new
-	end
-	
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
   def create
-   # @portfolio_item = Portfolio.new(portfolio_params)
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
-      	if @portfolio_item.save
-        	format.html { redirect_to portfolios_path, notice: "Your Portfolio item is noe live." }
-        	#format.json { render :show, status: :created, location: @portfolio_item }
-      	else
-        	format.html { render :new, status: :unprocessable_entity }
-        	#format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-    	  end
-    	end
-  	end
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
-  	def edit
-      @portfolio_item =Portfolio.find(params[:id])
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
+    @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
-      @portfolio_item =Portfolio.find(params[:id])
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: "Blog was successfully updated." }
-       # format.json { render :show, status: :ok, location: @blog }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        #format.json { render json: @blog.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
 
   def show
-    @portfolio_item =Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
-def destroy
-  #Perform the lookup
-  @portfolio_item =Portfolio.find(params[:id])
-  #Destroy/Delete the Recort
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
     @portfolio_item.destroy
 
-  #Redirect
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: "Record was Remove" }
- end
-end
-
-def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
 
 end
